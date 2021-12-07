ffb077e76caf9d5b58d3691580f486d139095b1f
SpicyTaterTot00 <mcfarland_jessica1@columbusstate.edu>
2021-12-07 19:17:20 +0000

    Adding image and public folder for Nav

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const axios = require('axios').default;
 var app= express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
+app.use( express.static( "public" ) );
 
 
 app.get('/', function(request, response){
