db2db1252a966d64d58daafd094a518d4b6204b3
SpicyTaterTot00 <mcfarland_jessica1@columbusstate.edu>
2021-12-08 15:14:38 +0000

    carousel added

--- index.js
+++ index.js
@@ -15,24 +15,29 @@ app.get('/', function(request, response){
 
 app.post('/', function(req,res){
     
-    axios.get(`https://newsapi.org/v2/everything?q=${req.body.keyword}&from=${req.body.date}&apikey=1d495c7b5c89457c8760dc87f4da8a1f
-    `)
-    .then(function(response){
+   axios.get(`https://newsapi.org/v2/everything?q=${req.body.keyword}&from=${req.body.date}&apikey=1d495c7b5c89457c8760dc87f4da8a1f
+   `)
+   .then(function(response){
 
-        newsdata = response.data
-        //res.json(newsdata);
+       
+       newsdata = response.data
+       
+
+
+        res.json(newsdata);
         console.log(newsdata);
-        //console.log(JSON.stringify(newsdata));
-        //res.json(JSON.stringify(newsdata));
-        
-        res.redirect('/');
-    })
-    .catch(function(error){
-        console.log(error);
-        res.redirect('/');
-    })
+        console.log(JSON.stringify(newsdata));
+        res.json(JSON.stringify(newsdata));
+       console.log(newsdata)
+       res.redirect('/');
+   })
+   .catch(function(error){
+       console.log(error);
+       res.redirect('/');
+   })
     
 });
+
 
 
 
